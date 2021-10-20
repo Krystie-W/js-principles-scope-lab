a3c2cc09392bf838a1c5272997f11aae967292a4
Krystie-W <tsukio.scoring@gmail.com>
2021-10-21 09:33:33 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+var customerName = 'bob';
+console.log(customerName);
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+console.log(upperCaseCustomerName());
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+console.log(setBestCustomer());
+console.log(bestCustomer);
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+console.log(overwriteBestCustomer());
+
+console.log(bestCustomer);
+
+const leastFavoriteCustomer = 'Jennifer';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Toby';
+    return leastFavoriteCustomer;
+}
+
+console.log(changeLeastFavoriteCustomer());
